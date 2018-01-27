08adff95c72c610e621ff3910d3ecbd322e2feb8
木戸 康平 <bouningenn@gmail.com>
2018-01-27 19:18:49 +0900

    add sion for test

--- test/obniz/index.js
+++ test/obniz/index.js
@@ -1,12 +1,19 @@
 var chai = require('chai');
 var assert = chai.assert;
+require('mocha-sinon');
 var Obniz = require("../../index.js");
 
 
-describe("obniz", function() {
+describe("obniz.index", function() {
+  beforeEach(function() {
+    this.sinon.stub(console, 'error');
+  });
+  
     it("instance", function() {
         var obniz = new Obniz("OBNIZ_ID_HERE");
-        assert.strictEqual(obniz, null);
+        assert(obniz instanceof Obniz , "create instance");
+        assert( console.error.calledOnce , "call once") ;
+        assert( console.error.calledWith("invalid obniz id"), "display error") ;
     });
 
 });
