dec90a38661079bdad9ccbbb1a378ff0ba2e3510
木戸 康平 <bouningenn@gmail.com>
2018-01-30 14:32:41 +0900

    adding test

--- test/index.js
+++ test/index.js
@@ -1,1 +1,6 @@
+
+
+var path = require('path');
+global.appRoot = path.resolve(__dirname + "/../") + "/";
+
 require( 'mocha-directory' )();
--- test/obniz/index.js
+++ test/obniz/index.js
@@ -1,26 +1,83 @@
 var chai = require('chai');
 var assert = chai.assert;
+var expect = chai.expect;
 var sinon = require('sinon');
 
-var projectBaseUrl = "../../";
-//require(projectBaseUrl + "/obniz/libs/io_.js");
-//var Obniz = require(projectBaseUrl + "/obniz/index.js");
-var Obniz = require(projectBaseUrl + "index.js");
+var Obniz = require(global.appRoot + "index.js");
+var WSServer = require('ws').Server;
 
-
+var debugLog = console.log.bind(console);
 describe("obniz.index", function () {
   beforeEach(function () {
+
     sinon.stub(console, 'error');
+    sinon.stub(console, 'log');
+
   });
+
   afterEach(function () {
     console.error.restore(); // Unwraps the spy
+    console.log.restore(); // Unwraps the spy
   });
 
   it("instance", function () {
     var obniz = new Obniz("OBNIZ_ID_HERE");
-    assert(obniz instanceof Obniz, "create instance");
-    assert(console.error.calledOnce, "call once");
-    assert(console.error.calledWith("invalid obniz id"), "display error");
-  });   
+    expect(obniz).to.be.instanceof(Obniz);
+    sinon.assert.calledOnce(console.error);
+    sinon.assert.calledWith(console.error, "invalid obniz id");
+  });
+
+  it("connect", async function () {
+    var port = 3001;
+    var server = createServer(port);
+    var obniz = new Obniz("11111111", {obniz_server: "ws://localhost:" + port});
     
+    await obniz.wait(100);    
+    expect(obniz).to.be.instanceof(Obniz);
+    expect(server.clients.size).to.equal(1);
+    obniz.close();
+    server.close();
+  });
+
 });
+
+function createServer(port, velify) {
+
+  var wss = new WSServer({host: "localhost", port: port, clientTracking: true});
+
+  if (velify === null || velify === undefined || velify === true) {
+    wss.verifyClient = function (info, accept) {
+      accept(true);
+    };
+  } else {
+    wss.verifyClient = function (info, accept) {
+      accept(false, velify.statusCode, velify.message);
+    };
+  }
+  wss.on('connection', function () {
+    debugLog("connect");
+  });
+  return wss;
+}
+
+
+
+//
+//var server = new WSServer({port:4000});
+//server.verifyClient = function(info, accept){
+//  accept(false, 500, "DataBase Error");
+//};
+//
+//server.verifyClient = function(info, accept){
+//  var obniz_id = parseObnizId(info.req);
+//  accept(false, 404, "obniz "+obniz_id+" is not exist");
+//};
+//
+//server.verifyClient = function(info, accept){
+//  accept(false, 302, "host="+obniz.wsredirect);
+//};
+//
+//server.verifyClient = function(info, accept){
+//  var obniz_id = parseObnizId(info.req);
+//  accept(false, 404, "obniz "+obniz_id+" is not online");
+//};
