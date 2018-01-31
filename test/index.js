dca68b21f357a677008f5e7ef042d0ba09bfe1b5
木戸 康平 <bouningenn@gmail.com>
2018-01-31 10:13:13 +0900

    adding io test

--- test/index.js
+++ test/index.js
@@ -3,7 +3,8 @@
 var sinon = require('sinon');
 var chai = require('chai');
 var expect = chai.expect;
-var WSServer = require('ws').Server;
+var ws = require('ws');
+var WSServer = ws.Server;
 var path = require('path');
 
 global.appRoot = path.resolve(__dirname + "/../") + "/";
@@ -34,44 +35,70 @@ var testUtil = {
   },
 
   setupObnizPromise: function (obj, done ) {
-    
-    new Promise(function (resolve) {
-//      this.log("setup");
-//      sinon.stub(console, 'error');
-//      sinon.stub(console, 'log');
-      var currentPort = port++;
-      obj.server = this.createServer(currentPort);
-      obj.server.on('connection', function () {
-//        this.log("resolve");
-        resolve();
-      }.bind(this));
-      obj.obniz = this.createObniz(currentPort, "12345678");
-    }.bind(this))
-        .then(function () {
-//          this.log("set Message callback");
-          expect(obj.server.clients.size).to.equal(1);
-          obj.onServerMessage = sinon.stub();
-          obj.server.clients.values().next().value.on("message", obj.onServerMessage);
-          return obj.obniz.wait(1);
-    }.bind(this)).then(function(){done();}.bind(this));
+    var stub = spyOnModule('ws');
+    stub.prototype.on = sinon.stub();
+    stub.prototype.send = sinon.stub();
+    stub.prototype.close = sinon.stub();
+    stub.prototype.removeAllListeners = sinon.stub();
+    obj.obniz = this.createObniz(100, "12345678");
+    obj.obniz.wsOnOpen();
+    done();
   },
 
   releaseObnizePromise: function (obj,done) {
-//    this.log("releaseObnizePromise");
     obj.obniz.close();
     obj.obniz = null;
     
-    obj.server.close();
-    obj.server = null;
     done();
-
-//    console.error.restore(); // Unwraps the spy
-//    console.log.restore(); // Unwraps the spy
-
+  },
+  
+  waitForWebsocketCall : function(obj, n){
+    return new Promise(function(resolve, reject){
+      var count = 100;
+      var wait = function(){
+        if(obj.onServerMessage.callCount >= n ){
+         resolve();
+        }else{
+          count--;
+          if(count < 0){
+            reject("waitForWebsocketCall timeout");return;
+          }
+          setTimeout(wait,10);
+          
+        }
+      };
+      setTimeout(wait,10);
+    });
+    
   }
+  
 
 };
 
 global.testUtil = testUtil;
+
+var moduleSpies = {};
+var originalJsLoader = require.extensions['.js'];
+
+spyOnModule = function spyOnModule(module) {
+  var path          = require.resolve(module);
+  var spy           = sinon.stub();
+  moduleSpies[path] = spy;
+  delete require.cache[path];
+  return spy;
+};
+
+require.extensions['.js'] = function (obj, path) {
+  if (moduleSpies[path])
+    obj.exports = moduleSpies[path];
+  else
+    return originalJsLoader(obj, path);
+}
+
+afterEach(function() {
+  for (var path in moduleSpies) {
+    delete moduleSpies[path];
+  }
+});
 
 require('mocha-directory')();
