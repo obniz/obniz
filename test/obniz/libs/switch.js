860aa98f106f34a8d495d218b044fc84b4af6357
木戸 康平 <bouningenn@gmail.com>
2018-02-04 12:07:15 +0900

    add switch test

---
+++ test/obniz/libs/switch.js
@@ -0,0 +1,65 @@
+var chai = require('chai');
+var assert = chai.assert;
+var expect = chai.expect;
+var sinon = require('sinon');
+var testUtil = require(global.appRoot + "/test/testUtil.js");
+chai.use(require('chai-like'));
+chai.use(testUtil.obnizAssert);
+
+describe("obniz.libs.switch", function () {
+  beforeEach(function (done) {
+    return testUtil.setupObnizPromise(this,done);
+    
+  });
+  
+  afterEach(function (done) {
+    return testUtil.releaseObnizePromise(this,done);
+  });
+  
+  it("onchange", function () {
+    var stub = sinon.stub();
+    this.obniz.switch.onchange = stub;
+    expect(this.obniz).to.be.obniz;
+    sinon.assert.callCount(stub, 0);
+    
+    testUtil.receiveJson(this.obniz,  {"switch":{"state":"none"}});
+    
+    sinon.assert.callCount(stub, 1);
+    expect(stub.getCall(0).args[0]).to.be.equal("none");
+    
+    expect(this.obniz).to.be.finished;
+  });
+  
+  it.skip("onchange changeしてないのにaction:getに反応しちゃってる", function () {
+    var stub = sinon.stub();
+    this.obniz.switch.onchange = stub;
+    expect(this.obniz).to.be.obniz;
+    sinon.assert.callCount(stub, 0);
+    
+    testUtil.receiveJson(this.obniz,  {"switch":{"state":"push", "action":"get"}});
+    
+    sinon.assert.callCount(stub, 0);
+    
+    expect(this.obniz).to.be.finished;
+  });
+  
+  it("inputWaitLeft", function () {
+    
+    return new Promise(function(resolve, reject){
+      this.obniz.switch.getWait().then(function(result){
+        expect(result).to.be.equal("left");  
+        resolve();
+      });
+
+      expect(this.obniz).to.be.obniz;
+      expect(this.obniz).send({"switch":"get"});  
+        expect(this.obniz).to.be.finished;
+
+      setTimeout(function(){    
+        testUtil.receiveJson(this.obniz,  {"switch":{"state":"left", "action":"get"}});
+      }.bind(this),10);
+    }.bind(this));
+    
+  });
+  
+});
