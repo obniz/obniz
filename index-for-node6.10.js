da27ca3a5a7554492b532d56f0075dd9309f1bea
yukisato <yuki@yuki-sato.com>
2018-02-06 14:59:41 +0900

    added chain method to WS2811

--- index-for-node6.10.js
+++ index-for-node6.10.js
@@ -3254,41 +3254,58 @@ class WS2811 {
   constructor() {}
 
   wired(obniz, din, nc0, nc1) {
+
     this.obniz = obniz;
     obniz.getIO(din).outputType("push-pull3v");
     this.spi = obniz.spi0; // TODO:
     this.spi.start("master", nc0, din, nc1, 2 * 1000 * 1000);
   }
 
-  rgb(r, g, b) {
-    var generateFromByte = function (val) {
-      val = parseInt(val);
-      const zero = 0x8;
-      const one = 0xE;
-      let ret = [];
-      for (var i = 0; i < 8; i += 2) {
-        let byte = 0;
-        if (val & 0x80 >> i) {
-          byte = one << 4;
-        } else {
-          byte = zero << 4;
-        }
-        if (val & 0x80 >> i + 1) {
-          byte |= one;
-        } else {
-          byte |= zero;
-        }
-        ret.push(byte);
-      }
-      return ret;
-    };
+  static _generateFromByte(val) {
 
-    let array = generateFromByte(r);
-    array = array.concat(generateFromByte(g));
-    array = array.concat(generateFromByte(b));
-    console.log(array);
-    this.spi.write(array);
+    val = parseInt(val);
+    const zero = 0x8;
+    const one = 0xE;
+    let ret = [];
+    for (var i = 0; i < 8; i += 2) {
+      let byte = 0;
+      if (val & 0x80 >> i) {
+        byte = one << 4;
+      } else {
+        byte = zero << 4;
+      }
+      if (val & 0x80 >> i + 1) {
+        byte |= one;
+      } else {
+        byte |= zero;
+      }
+      ret.push(byte);
+    }
+    return ret;
   }
+
+  static _generateColor(r, g, b) {
+
+    let array = WS2811._generateFromByte(r);
+    array = array.concat(WS2811._generateFromByte(g));
+    array = array.concat(WS2811._generateFromByte(b));
+    return array;
+  }
+
+  rgb(r, g, b) {
+
+    this.spi.write(_generateColor(r, g, b));
+  }
+
+  rgbs(array) {
+    let bytes = [];
+    for (var i = 0; i < array.length; i++) {
+      const oneArray = array[i];
+      bytes = bytes.concat(WS2811._generateColor(oneArray[0], oneArray[1], oneArray[2]));
+    }
+    this.spi.write(bytes);
+  }
+
 }
 
 if (PartsRegistrate) {
--- index.js
+++ index.js
@@ -3363,41 +3363,58 @@ class WS2811 {
   }
 
   wired(obniz, din ,nc0, nc1){
+
     this.obniz = obniz;
     obniz.getIO(din).outputType("push-pull3v");
     this.spi = obniz.spi0;// TODO:
     this.spi.start("master", nc0, din, nc1, 2*1000*1000); 
   };
 
-  rgb(r, g, b){
-    var generateFromByte = function(val) {
-      val = parseInt(val);
-      const zero = 0x8;
-      const one  = 0xE;
-      let ret = [];
-      for (var i=0; i<8;i+=2) {
-        let byte = 0;
-        if (val & (0x80 >> i)) {
-          byte = one << 4;
-        } else {
-          byte = zero << 4;
-        }
-        if (val & (0x80 >> (i+1))) {
-          byte |= one
-        } else {
-          byte |= zero
-        }
-        ret.push(byte);
+  static _generateFromByte(val) {
+
+    val = parseInt(val);
+    const zero = 0x8;
+    const one  = 0xE;
+    let ret = [];
+    for (var i=0; i<8;i+=2) {
+      let byte = 0;
+      if (val & (0x80 >> i)) {
+        byte = one << 4;
+      } else {
+        byte = zero << 4;
       }
-      return ret;
+      if (val & (0x80 >> (i+1))) {
+        byte |= one
+      } else {
+        byte |= zero
+      }
+      ret.push(byte);
     }
-  
-    let array = generateFromByte(r);
-    array = array.concat(generateFromByte(g));
-    array = array.concat(generateFromByte(b));
-    console.log(array);
-    this.spi.write(array);
+    return ret;
   }
+
+  static _generateColor(r, g, b) {
+  
+    let array = WS2811._generateFromByte(r);
+    array = array.concat(WS2811._generateFromByte(g));
+    array = array.concat(WS2811._generateFromByte(b));
+    return array;
+  }
+
+  rgb(r, g, b){
+
+    this.spi.write(_generateColor(r, g, b));
+  }
+
+  rgbs(array) {
+    let bytes = [];
+    for (var i=0; i<array.length; i++) {
+      const oneArray = array[i];
+      bytes = bytes.concat(WS2811._generateColor(oneArray[0], oneArray[1], oneArray[2]));
+    }
+    this.spi.write(bytes);
+  }
+
 }
 
 if (PartsRegistrate) {
