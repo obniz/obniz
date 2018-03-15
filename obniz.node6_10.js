2d56327aeea2cbe6f7859acc0bf1e9d1a87851d0
木戸 康平 <bouningenn@gmail.com>
2018-03-15 16:05:25 +0900

    fix sht31

--- obniz.node6_10.js
+++ obniz.node6_10.js
@@ -7170,8 +7170,28 @@ if (PartsRegistrate) {
 }
 
 var SHT31 = function () {
-  this.requiredKeys = ["adr", "addressmode", "i2c"];
+  this.requiredKeys = ["adr", "addressmode"];
   this.keys = ["vcc", "sda", "scl", "gnd", "adr", "addressmode", "i2c"];
+  this.ioKeys = ["vcc", "sda", "scl", "gnd", "adr"];
+
+  this.commands = {};
+  this.commands.softReset = [0x30, 0xA2];
+  this.commands.highRepeatStreach = [0x2C, 0x06];
+  this.commands.middleRepeatStreach = [0x2C, 0x0D];
+  this.commands.lowRepeatStreach = [0x2C, 0x10];
+  this.commands.highRepeat = [0x24, 0x00];
+  this.commands.mediumRepeat = [0x24, 0x0B];
+  this.commands.lowRepeat = [0x24, 0x16];
+
+  this.waitTime = {};
+  this.waitTime.wakeup = 1;
+  this.waitTime.softReset = 1;
+  this.waitTime.lowRepeat = 4;
+  this.waitTime.mediumRepeat = 6;
+  this.waitTime.highRepeat = 15;
+
+  //not tested
+  this.commands.readStatus = [0xF3, 0x2D];
 };
 
 SHT31.prototype.wired = function (obniz) {
@@ -7191,25 +7211,32 @@ SHT31.prototype.wired = function (obniz) {
   this.params.mode = this.params.mode || "master"; //for i2c
   this.params.pullType = this.params.pullType || "float"; //for i2c
   this.i2c = obniz.getI2CWithConfig(this.params);
-  //obniz.i2c0.write(address, [0x20, 0x24]);
+  obniz.i2c0.write(this.address, this.commands.softReset);
 };
 
+SHT31.prototype.getData = _asyncToGenerator(function* () {
+  this.i2c.write(this.address, this.commands.highRepeat);
+  yield obniz.freeze(this.waitTime.highRepeat);
+  return yield this.i2c.readWait(this.address, 6);
+});
+
 SHT31.prototype.getTempWait = _asyncToGenerator(function* () {
-  this.i2c.write(this.address, [0x20, 0x24]);
-  this.i2c.write(this.address, [0xE0, 0x00]);
-  var ret = yield this.i2c0.readWait(this.address, 4);
-  var tempBin = ret[0].toString(2) + ('00000000' + ret[1].toString(2)).slice(-8);
-  var temperature = -45 + 175 * (parseInt(tempBin, 2) / (65536 - 1));
-  return temperature;
+  return (yield this.getAllWait()).temperature;
 });
 
 SHT31.prototype.getHumdWait = _asyncToGenerator(function* () {
-  this.i2c.i2c0.write(this.address, [0x20, 0x24]);
-  this.i2c.i2c0.write(this.address, [0xE0, 0x00]);
-  var ret = yield this.i2c.i2c0.readWait(this.address, 4);
-  var humdBin = ret[2].toString(2) + ('00000000' + ret[3].toString(2)).slice(-8);
-  var humidity = 100 * (parseInt(humdBin, 2) / (65536 - 1));
-  return humidity;
+  return (yield this.getAllWait()).humidity;
+});
+
+SHT31.prototype.getAllWait = _asyncToGenerator(function* () {
+  let ret = yield this.getData();
+
+  let tempBin = ret[0] * 256 + ret[1];
+  let temperature = -45 + 175 * (tempBin / (65536 - 1));
+
+  let humdBin = ret[3] * 256 + ret[4];
+  let humidity = 100 * (humdBin / (65536 - 1));
+  return { temperature, humidity };
 });
 
 if (PartsRegistrate) {
@@ -7222,7 +7249,7 @@ var ADT7310 = function () {
 };
 
 ADT7310.prototype.wired = (() => {
-  var _ref17 = _asyncToGenerator(function* (obniz) {
+  var _ref19 = _asyncToGenerator(function* (obniz) {
     this.obniz = obniz;
 
     obniz.setVccGnd(this.params.vcc, this.params.gnd, "5v");
@@ -7235,7 +7262,7 @@ ADT7310.prototype.wired = (() => {
   });
 
   return function (_x6) {
-    return _ref17.apply(this, arguments);
+    return _ref19.apply(this, arguments);
   };
 })();
 
