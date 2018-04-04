5927e053f66b49fb2dfe4cf635c299815b16c18b
yukisato <yuki@yuki-sato.com>
2018-04-04 18:25:15 +0900

    0.1.46

--- obniz.node6_10.js
+++ obniz.node6_10.js
@@ -2,7 +2,7 @@
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var _obniz_js_version = "0.1.45";
+var _obniz_js_version = "0.1.46";
 
 /* global showObnizDebugError  */
 
