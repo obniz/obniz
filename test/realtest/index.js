5a19f637748d9628006f2d3c812aa81bbbd78af5
yukisato <yuki@yuki-sato.com>
2018-04-06 14:21:30 +0900

    separate file

---
+++ test/realtest/index.js
@@ -0,0 +1,44 @@
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const sinon = require('sinon');
+const path = require('path');
+const fs = require('fs');
+
+const Obniz = require('../../index.js');
+
+describe("obniz.index", function () {
+
+  var obnizA;
+  var obnizB;
+
+  beforeEach(function (done) {
+
+    obnizA = new Obniz("10760979");
+    //obnizA.debugprint = true;
+    obnizA.onconnect = () => {
+      obnizB = new Obniz("00978479");
+      //obnizB.debugprint = true;
+      obnizB.onconnect = ()=>{
+        done();
+      }
+    }
+
+  });
+
+  afterEach(function () {
+    //obnizA.close();
+    //obnizB.close();
+  });
+
+  const files = fs.readdirSync(path.join(__dirname, 'tests'));
+  for (let i=0; i<files.length; i++) {
+    if (files[i].indexOf(".js") >=0) {
+      it("tests/"+files[i], async function () {
+        const test = require(path.join(__dirname, 'tests', files[i]))
+        test({obnizA, obnizB});
+      });
+    }
+  }
+
+});
