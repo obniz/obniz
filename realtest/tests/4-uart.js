6fde64ecdaacc8d8262fd0a0428566fa4a0b0c8c
yukisato <yuki@yuki-sato.com>
2018-04-07 14:48:41 +0900

    remove unused

--- realtest/tests/4-uart.js
+++ realtest/tests/4-uart.js
@@ -191,10 +191,4 @@ module.exports = async function(config) {
     
   });
   
-  async function ioBisInRange(io, range) {
-    await obnizA.pingWait();
-    var voltage = await obnizB.getAD(io).getWait();
-    expect(voltage).to.be.within(range[0], range[1]);
-  }
-
 }
