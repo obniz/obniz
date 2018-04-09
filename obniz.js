8624abd289e5ff1211811f6cb68c2da431e4ceb7
yukisato <yuki@yuki-sato.com>
2018-04-09 13:08:57 +0900

    ble error should pass to obniz.error

--- obniz.js
+++ obniz.js
@@ -7174,16 +7174,22 @@ class ObnizBLE {
 
     if (obj.error) {
       let params = obj.error;
-        if (!params.address){
-           if(typeof(this.onerror) === "function"){
-             this.onerror(params);
-           }
-        }
-         
-        let p = this.findPeripheral(params.address);
-        if (p) {
-          p.onerror(params);
-        }
+      let handled = false;
+      if (!params.address){
+          if(typeof(this.onerror) === "function"){
+            this.onerror(params);
+            handled = true;
+          }
+      }
+        
+      let p = this.findPeripheral(params.address);
+      if (p) {
+        p.onerror(params);
+        handled = true;
+      }
+      if (!handled) {
+        this.Obniz.error(`ble ${params.message} service=${params.service_uuid} characteristic_uuid=${params.characteristic_uuid} descriptor_uuid=${params.descriptor_uuid}`);
+      }
     }
   }
 
