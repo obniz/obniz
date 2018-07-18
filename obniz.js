f73e22c6b1d735b027ed452ef07e4f5d52e52b32
yukisato <yuki@yuki-sato.com>
2018-07-18 19:54:27 +0900

    added BME280

--- obniz.js
+++ obniz.js
@@ -37,32 +37,17 @@ var Obniz =
 /******/ 	// define getter function for harmony exports
 /******/ 	__webpack_require__.d = function(exports, name, getter) {
 /******/ 		if(!__webpack_require__.o(exports, name)) {
-/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
 /******/ 		}
 /******/ 	};
 /******/
 /******/ 	// define __esModule on exports
 /******/ 	__webpack_require__.r = function(exports) {
-/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 		}
 /******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 	};
-/******/
-/******/ 	// create a fake namespace object
-/******/ 	// mode & 1: value is a module id, require it
-/******/ 	// mode & 2: merge all properties of value into the ns
-/******/ 	// mode & 4: return value when already ns object
-/******/ 	// mode & 8|1: behave like require
-/******/ 	__webpack_require__.t = function(value, mode) {
-/******/ 		if(mode & 1) value = __webpack_require__(value);
-/******/ 		if(mode & 8) return value;
-/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-/******/ 		var ns = Object.create(null);
-/******/ 		__webpack_require__.r(ns);
-/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-/******/ 		return ns;
 /******/ 	};
 /******/
 /******/ 	// getDefaultExport function for compatibility with non-harmony modules
@@ -239,12 +224,13 @@ var map = {
 
 function webpackContext(req) {
 	var id = webpackContextResolve(req);
-	return __webpack_require__(id);
+	var module = __webpack_require__(id);
+	return module;
 }
 function webpackContextResolve(req) {
 	var id = map[req];
 	if(!(id + 1)) { // check for number or string
-		var e = new Error("Cannot find module '" + req + "'");
+		var e = new Error('Cannot find module "' + req + '".');
 		e.code = 'MODULE_NOT_FOUND';
 		throw e;
 	}
@@ -7230,7 +7216,7 @@ module.exports = g;
 /***/ (function(module, exports) {
 
 function webpackEmptyContext(req) {
-	var e = new Error("Cannot find module '" + req + "'");
+	var e = new Error('Cannot find module "' + req + '".');
 	e.code = 'MODULE_NOT_FOUND';
 	throw e;
 }
@@ -13902,7 +13888,7 @@ module.exports = ws;
 /***/ (function(module, exports) {
 
 function webpackEmptyContext(req) {
-	var e = new Error("Cannot find module '" + req + "'");
+	var e = new Error('Cannot find module "' + req + '".');
 	e.code = 'MODULE_NOT_FOUND';
 	throw e;
 }
@@ -17438,7 +17424,7 @@ module.exports = JsonBinaryConverter;
 /***/ "./package.json":
 /***/ (function(module) {
 
-module.exports = {"name":"obniz","version":"1.7.2","description":"obniz sdk for javascript","main":"index.js","scripts":{"test":"nyc --reporter=text --reporter=html mocha $NODE_DEBUG_OPTION  ./test/index.js","buildAndtest":"npm run build && npm test","realtest":"mocha $NODE_DEBUG_OPTION -b ./realtest/index.js","local":"gulp --gulpfile ./_tools/server.js --cwd .","build":"npm run lint && gulp $NODE_DEBUG_OPTION --gulpfile ./_tools/server.js --cwd . build","version":"npm run build && git add obniz.js && git add obniz.min.js && git add obniz.node6_10.js","lint":"eslint --fix .","precommit":"lint-staged"},"lint-staged":{"*.js":["eslint --fix","git add"]},"keywords":["obniz"],"repository":"obniz/obniz","author":"yukisato <yuki@yuki-sato.com>","homepage":"https://obniz.io/","license":"SEE LICENSE IN LICENSE.txt","devDependencies":{"babel-cli":"^6.26.0","babel-core":"^6.26.3","babel-loader":"^7.1.4","babel-polyfill":"^6.26.0","babel-preset-env":"^1.7.0","babel-preset-es2015":"^6.24.1","babel-preset-stage-3":"^6.24.1","chai":"^4.1.2","chai-like":"^1.1.1","child_process":"^1.0.2","chokidar":"^1.7.0","concat-with-sourcemaps":"^1.1.0","ejs":"^2.6.1","eslint":"^4.19.1","eslint-config-prettier":"^2.9.0","eslint-plugin-jasmine":"^2.10.1","eslint-plugin-prettier":"^2.6.0","express":"^4.16.2","get-port":"^3.2.0","glob":"^7.1.2","gulp":"^3.9.1","gulp-babel":"^7.0.1","gulp-concat":"^2.6.1","gulp-ejs":"^3.1.3","gulp-filter":"^5.1.0","gulp-notify":"^3.2.0","gulp-plumber":"^1.2.0","gulp-sort":"^2.0.0","gulp-util":"^3.0.8","gulp-yaml":"^1.0.1","husky":"^0.14.3","json-loader":"^0.5.7","lint-staged":"^7.2.0","mocha":"^5.2.0","mocha-chrome":"^1.1.0","mocha-directory":"^2.3.0","mocha-sinon":"^2.1.0","ncp":"^2.0.0","node-notifier":"^5.2.1","nyc":"^11.9.0","path":"^0.12.7","prettier":"^1.13.5","sinon":"^4.5.0","svg-to-png":"^3.1.2","through2":"^2.0.3","uglifyjs-webpack-plugin":"^1.2.5","vinyl":"^2.1.0","webpack":"^4.12.0","webpack-cli":"^2.1.5","webpack-node-externals":"^1.7.2","webpack-stream":"^4.0.3","yaml-loader":"^0.5.0"},"dependencies":{"eventemitter3":"^3.1.0","js-yaml":"^3.12.0","node-dir":"^0.1.17","node-fetch":"^2.1.2","semver":"^5.5.0","tv4":"^1.3.0","ws":"^5.2.0"},"bugs":{"url":"https://github.com/obniz/obniz/issues"},"private":false,"browser":{"ws":"./obniz/libs/webpackReplace/ws.js","canvas":"./obniz/libs/webpackReplace/canvas.js","./obniz/libs/webpackReplace/require-context.js":"./obniz/libs/webpackReplace/require-context-browser.js"}};
+module.exports = {"name":"obniz","version":"1.7.2","description":"obniz sdk for javascript","main":"index.js","scripts":{"test":"nyc --reporter=text --reporter=html mocha $NODE_DEBUG_OPTION  ./test/index.js","buildAndtest":"npm run build && npm test","realtest":"mocha $NODE_DEBUG_OPTION -b ./realtest/index.js","local":"gulp --gulpfile ./_tools/server.js --cwd .","build":"npm run lint && gulp $NODE_DEBUG_OPTION --gulpfile ./_tools/server.js --cwd . build","version":"npm run build && git add obniz.js && git add obniz.min.js && git add obniz.node6_10.js","lint":"eslint --fix .","precommit":"lint-staged"},"lint-staged":{"*.js":["eslint --fix","git add"]},"keywords":["obniz"],"repository":"obniz/obniz","author":"yukisato <yuki@yuki-sato.com>","homepage":"https://obniz.io/","license":"SEE LICENSE IN LICENSE.txt","devDependencies":{"babel-cli":"^6.26.0","babel-core":"^6.26.3","babel-loader":"^7.1.5","babel-polyfill":"^6.26.0","babel-preset-env":"^1.7.0","babel-preset-es2015":"^6.24.1","babel-preset-stage-3":"^6.24.1","chai":"^4.1.2","chai-like":"^1.1.1","child_process":"^1.0.2","chokidar":"^1.7.0","concat-with-sourcemaps":"^1.1.0","ejs":"^2.6.1","eslint":"^4.19.1","eslint-config-prettier":"^2.9.0","eslint-plugin-jasmine":"^2.10.1","eslint-plugin-prettier":"^2.6.2","express":"^4.16.2","get-port":"^3.2.0","glob":"^7.1.2","gulp":"^3.9.1","gulp-babel":"^7.0.1","gulp-concat":"^2.6.1","gulp-ejs":"^3.1.3","gulp-filter":"^5.1.0","gulp-notify":"^3.2.0","gulp-plumber":"^1.2.0","gulp-sort":"^2.0.0","gulp-util":"^3.0.8","gulp-yaml":"^1.0.1","husky":"^0.14.3","json-loader":"^0.5.7","lint-staged":"^7.2.0","mocha":"^5.2.0","mocha-chrome":"^1.1.0","mocha-directory":"^2.3.0","mocha-sinon":"^2.1.0","ncp":"^2.0.0","node-notifier":"^5.2.1","nyc":"^11.9.0","path":"^0.12.7","prettier":"^1.13.5","sinon":"^4.5.0","svg-to-png":"^3.1.2","through2":"^2.0.3","uglifyjs-webpack-plugin":"^1.2.7","vinyl":"^2.1.0","webpack":"^4.12.0","webpack-cli":"^2.1.5","webpack-node-externals":"^1.7.2","webpack-stream":"^4.0.3","yaml-loader":"^0.5.0"},"dependencies":{"eventemitter3":"^3.1.0","js-yaml":"^3.12.0","node-dir":"^0.1.17","node-fetch":"^2.1.2","semver":"^5.5.0","tv4":"^1.3.0","ws":"^5.2.2"},"bugs":{"url":"https://github.com/obniz/obniz/issues"},"private":false,"browser":{"ws":"./obniz/libs/webpackReplace/ws.js","canvas":"./obniz/libs/webpackReplace/canvas.js","./obniz/libs/webpackReplace/require-context.js":"./obniz/libs/webpackReplace/require-context-browser.js"}};
 
 /***/ }),
 
@@ -17490,6 +17476,7 @@ var map = {
 	"./TemperatureSensor/analog/S8100B/index.js": "./parts/TemperatureSensor/analog/S8100B/index.js",
 	"./TemperatureSensor/analog/S8120C/index.js": "./parts/TemperatureSensor/analog/S8120C/index.js",
 	"./TemperatureSensor/i2c/ADT7410/index.js": "./parts/TemperatureSensor/i2c/ADT7410/index.js",
+	"./TemperatureSensor/i2c/BME280/index.js": "./parts/TemperatureSensor/i2c/BME280/index.js",
 	"./TemperatureSensor/i2c/S-5851A/index.js": "./parts/TemperatureSensor/i2c/S-5851A/index.js",
 	"./TemperatureSensor/i2c/SHT31/index.js": "./parts/TemperatureSensor/i2c/SHT31/index.js",
 	"./TemperatureSensor/spi/ADT7310/index.js": "./parts/TemperatureSensor/spi/ADT7310/index.js",
@@ -17500,12 +17487,13 @@ var map = {
 
 function webpackContext(req) {
 	var id = webpackContextResolve(req);
-	return __webpack_require__(id);
+	var module = __webpack_require__(id);
+	return module;
 }
 function webpackContextResolve(req) {
 	var id = map[req];
 	if(!(id + 1)) { // check for number or string
-		var e = new Error("Cannot find module '" + req + "'");
+		var e = new Error('Cannot find module "' + req + '".');
 		e.code = 'MODULE_NOT_FOUND';
 		throw e;
 	}
@@ -20771,6 +20759,251 @@ class ADT7410 {
 
 if (true) {
   module.exports = ADT7410;
+}
+
+
+/***/ }),
+
+/***/ "./parts/TemperatureSensor/i2c/BME280/index.js":
+/***/ (function(module, exports, __webpack_require__) {
+
+class BME280 {
+  constructor() {
+    this.requiredKeys = [];
+    this.keys = [
+      'vcore',
+      'vio',
+      'gnd',
+      'csb',
+      'sdi',
+      'sck',
+      'sdo',
+      'i2c',
+      'address'
+    ];
+
+    this.ioKeys = ['vcore', 'vio', 'gnd', 'csb', 'sdi', 'sdo', 'sck'];
+
+    this.configration = {
+      sampling: {
+        temp:1, // 0 never. 0b001 to 0b101
+        hum: 1,
+        pres: 1
+      },
+      interval: 5,    // 0b000 to 0b111
+      iir_strength: 0,  // 000 to 100 (0=disable)
+      mode: 3,
+
+      Modes: {
+        sleep: 0,
+        forced: 1, // or 2
+        normal: 3,
+      }
+    }
+
+    this.commands = {};
+
+    this.commands.addresses = {
+      config: 0xF5,
+      ctrl_meas: 0xF4,
+      ctrl_hum: 0xF2
+    }
+  }
+
+  static info() {
+    return {
+      name: 'BME280',
+      datasheet: "https://ae-bst.resource.bosch.com/media/_tech/media/datasheets/BST-BME280_DS001-12.pdf"
+    };
+  }
+
+  wired(obniz) {
+    this.obniz = obniz;
+
+    if (obniz.isValidIO(this.params.csb)) { // selecting I2C mode before powerup
+      this.io_csb = obniz.getIO(this.params.csb);
+      this.io_csb.drive("3v");
+      this.io_csb.output(true);
+    }
+
+    this.obniz.setVccGnd(this.params.vio, null, '3v');
+    this.obniz.setVccGnd(this.params.vcore, null, '3v');
+    this.obniz.setVccGnd(null, this.params.gnd, '5v');
+    this.obniz.wait(10);
+
+    this.address = 0x76;
+    if (this.params.address === 0x76) {
+      this.address = 0x76;
+    } else if (this.params.address === 0x77) {
+      this.address = 0x77;
+    } else if (this.params.address !== undefined ){
+      throw new Error('address must be 0x76 or 0x77');
+    }
+
+    if (obniz.isValidIO(this.params.sdo)) {
+      this.io_sdo = obniz.getIO(this.params.sdo);
+      this.io_sdo.drive("3v");
+      this.io_sdo.output( (this.address === 0x76) ? false : true )
+    }
+    
+    this.params.sda =  this.params.sda || this.params.sdi;
+    this.params.scl = this.params.scl || this.params.sck;
+    this.params.clock = this.params.clock || 100 * 1000;
+    this.params.mode = 'master';
+    this.params.pull = '3v';
+    this.i2c = obniz.getI2CWithConfig(this.params);
+
+    this.obniz.wait(10);
+
+    this.config();
+
+    this.obniz.wait(10);
+  }
+
+  async config() {
+    this.write([this.commands.addresses.config,     (this.configration.interval << 5) | (this.configration.iir_strength << 2) | 0]);
+    this.write([this.commands.addresses.ctrl_hum,    this.configration.sampling.hum]);
+    this.write([this.commands.addresses.ctrl_meas,  (this.configration.sampling.temp << 5) | (this.configration.sampling.pres << 2) | this.configration.mode]);
+  }
+
+  async setIIRStrength(strengh) {
+    this.configration.iir_strength = strengh;
+    this.config();
+  }
+
+  async applyCalibration() {
+    this.i2c.write(this.address, [0x88]);
+    let data = await this.i2c.readWait(this.address, 24);
+    this.i2c.write(this.address, [0xA1]);
+    let data_next = await this.i2c.readWait(this.address, 1);
+    data.push(... data_next);
+    this.i2c.write(this.address, [0xE1]);
+    data_next = await this.i2c.readWait(this.address, 7);
+    data.push(... data_next);
+    this._calibrated = {
+      dig_T1: (data[1] << 8) | data[0],
+      dig_T2: this._readSigned16((data[3] << 8) | data[2]),
+      dig_T3: this._readSigned16((data[5] << 8) | data[4]),
+      dig_P1: (data[7] << 8) | data[6],
+      dig_P2: this._readSigned16((data[9] << 8) | data[8]),
+      dig_P3: this._readSigned16((data[11]<< 8) | data[10]),
+      dig_P4: this._readSigned16((data[13]<< 8) | data[12]),
+      dig_P5: this._readSigned16((data[15]<< 8) | data[14]),
+      dig_P6: this._readSigned16((data[17]<< 8) | data[16]),
+      dig_P7: this._readSigned16((data[19]<< 8) | data[18]),
+      dig_P8: this._readSigned16((data[21]<< 8) | data[20]),
+      dig_P9: this._readSigned16((data[23]<< 8) | data[22]),
+      dig_H1: this._readSigned8(data[24]),
+      dig_H2: this._readSigned16((data[26]<< 8) | data[25]),
+      dig_H3: this._readSigned8(data[27]),
+      dig_H4: this._readSigned16((data[28]<< 4) | (0x0F & data[29])),
+      dig_H5: this._readSigned16((data[30] << 4) | ((data[29] >> 4) & 0x0F)),
+      dig_H6: this._readSigned8(data[31])
+    }
+    this._t_fine = 0;
+  }
+
+  _readSigned16(value) {
+    if (value >= 0x8000) {
+      value = value - 0x10000;
+    }
+    return value;
+  }
+  _readSigned8(value) {
+    if (value >= 0x80) {
+      value = value - 0x100;
+    }
+    return value;
+  }
+
+  write(data) {
+    this.obniz.i2c0.write(this.address, data);
+  }
+
+  async getData() {
+    this.i2c.write(this.address, [0xF7]);
+    return await this.i2c.readWait(this.address, 8);
+  }
+
+  async getAllWait() {
+    let data = await this.getData();
+
+    const press_raw = (data[0] << 12) | (data[1] << 4) | (data[2] >> 4);
+    const temp_raw = (data[3] << 12) | (data[4] << 4) | (data[5] >> 4);
+    const hum_raw  = (data[6] << 8) | data[7];
+
+    let temperature = this.calibration_T(temp_raw) / 100.0;
+    let pressure = this.calibration_P(press_raw) / 100.0;
+    let humidity = this.calibration_H(hum_raw);
+
+    return { temperature, humidity, pressure };
+  }
+
+  calibration_T(adc_T){
+    let var1, var2, T;
+    var1 = ((((adc_T >> 3) - (this._calibrated.dig_T1<<1))) * (this._calibrated.dig_T2)) >> 11;
+    var2 = (((((adc_T >> 4) - (this._calibrated.dig_T1)) * ((adc_T>>4) - (this._calibrated.dig_T1))) >> 12) * (this._calibrated.dig_T3)) >> 14;
+    
+    this._t_fine = var1 + var2;
+    T = (this._t_fine * 5 + 128) >> 8;
+    return T; 
+  }
+
+  calibration_P(adc_P){
+    let pvar1 = this._t_fine / 2 - 64000;
+    let pvar2 = pvar1 * pvar1 * this._calibrated.dig_P6 / 32768;
+    pvar2 = pvar2 + pvar1 * this._calibrated.dig_P5 * 2;
+    pvar2 = pvar2 / 4 + this._calibrated.dig_P4 * 65536;
+    pvar1 = (this._calibrated.dig_P3 * pvar1 * pvar1 / 524288 + this._calibrated.dig_P2 * pvar1) / 524288;
+    pvar1 = (1 + pvar1 / 32768) * this._calibrated.dig_P1;
+
+    if(pvar1 !== 0) {
+      let p = 1048576 - adc_P;
+      p = ((p - pvar2 / 4096) * 6250) / pvar1;
+      pvar1 = this._calibrated.dig_P9 * p * p / 2147483648;
+      pvar2 = p * this._calibrated.dig_P8 / 32768;
+      p = p + (pvar1 + pvar2 + this._calibrated.dig_P7) / 16;
+      return p;
+    }
+    return 0;
+  }
+
+  calibration_H(adc_H){
+    let h = this._t_fine - 76800;
+    h = (adc_H - (this._calibrated.dig_H4 * 64 + this._calibrated.dig_H5 / 16384 * h)) *
+        (this._calibrated.dig_H2 / 65536 * (1 + this._calibrated.dig_H6 / 67108864 * h * (1 + this._calibrated.dig_H3 / 67108864 * h)));
+    h = h * (1 - this._calibrated.dig_H1 * h / 524288);
+    return h;
+  }
+
+  async getTempWait() {
+    return (await this.getAllWait()).temperature;
+  }
+
+  async getHumdWait() {
+    return (await this.getAllWait()).humidity;
+  }
+
+  async getPressureWait() {
+    return (await this.getAllWait()).pressure;
+  }
+
+  async getAltitudeWait() {
+    const pressure = await this.getPressureWait();
+    return this.calcAltitude(pressure);
+  }
+
+  calcAltitude(pressure, seaLevel) {
+    if(!seaLevel) {
+      seaLevel = 1013.25;
+    }
+
+    return (1.0 - Math.pow(pressure / seaLevel, (1 / 5.2553))) * 145366.45 * 0.3048;
+  }
+}
+
+if (true) {
+  module.exports = BME280;
 }
 
 
