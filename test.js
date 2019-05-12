dd1510371ac2dad9c396694a053de92116db5fd9
kido <bouningenn@gmail.com>
2019-05-12 21:59:13 +0900

    Delete test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-let Obniz = require('./index.js');
-
-let obniz = new Obniz('5252-7518');
-obniz.onconnect = async function() {
-  let tempsens = obniz.wired('LM60', { gnd: 0, output: 1, vcc: 2 });
-  tempsens.onchange = function(temp) {
-    console.log(temp);
-  };
-};
