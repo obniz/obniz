89603fda4332477487a769edccb045eb34333cc0
takasago <vipqiv00@gmail.com>
2019-06-17 15:06:53 +0900

    ADD: typescript files

---
+++ parts/Grove/Grove_Button/index.d.ts
@@ -0,0 +1,11 @@
+export interface Grove_ButtonOptions {
+  signal: number;
+  vcc: number;
+  gnd?: number;
+}
+
+export interface Grove_Button {
+  onchange: (pressed: boolean) => void;
+  isPressedWait(): Promise<boolean>;
+  stateWait(pressed: boolean): Promise<void>;
+}
---
+++ parts/Grove/Grove_Buzzer/index.d.ts
@@ -0,0 +1,10 @@
+export interface Grove_BuzzerOprions {
+  signal?: number;
+  gnd: number;
+  vcc: number;
+}
+
+export interface Grove_Buzzer {
+  play(frequency: number): void;
+  stop(): void;
+}
