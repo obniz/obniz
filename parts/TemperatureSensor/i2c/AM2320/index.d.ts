3c29d7c10b29e2ceaf7c380c48d59676076ef7ed
waminet <wamiwami@live.jp>
2019-08-21 14:16:00 +0900

    fix i2c object

--- parts/TemperatureSensor/i2c/AM2320/index.d.ts
+++ parts/TemperatureSensor/i2c/AM2320/index.d.ts
@@ -1,9 +1,10 @@
+import { I2C } from '../../../../obniz/libs/io_peripherals/i2c';
 export interface AM2320ptions {
   vcc?: number;
   gnd?: number;
   sda?: number;
   scl?: number;
-  i2c?: any;
+  i2c?: I2C;
 }
 
 export interface AM2320 {
