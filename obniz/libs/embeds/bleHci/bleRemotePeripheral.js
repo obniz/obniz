7f66c4d3a9fb29c0081336198c0f2b21f883f137
木戸 康平 <bouningenn@gmail.com>
2019-12-17 11:52:59 +0900

    add pollyfill

--- obniz/libs/embeds/bleHci/bleRemotePeripheral.js
+++ obniz/libs/embeds/bleHci/bleRemotePeripheral.js
@@ -248,6 +248,22 @@ class BleRemotePeripheral {
   }
 
   async discoverAllHandlesWait(){
+    if (!Array.prototype.flat) {
+      Array.prototype.flat = function(depth) {
+        var flattend = [];
+        (function flat(array, depth) {
+          for (let el of array) {
+            if (Array.isArray(el) && depth > 0) {
+              flat(el, depth - 1);
+            } else {
+              flattend.push(el);
+            }
+          }
+        })(this, Math.floor(depth) || 1);
+        return flattend;
+      };
+    }
+
     let services = await this.discoverAllServicesWait();
     let charsNest = await Promise.all(services.map(s => s.discoverAllCharacteristicsWait()));
     let chars = charsNest.flat();
