e2c0c5a1be6174036f31d8de83023c4323e99d19
木戸 康平 <bouningenn@gmail.com>
2019-12-20 12:58:35 +0900

    remove warnings

--- obniz.js
+++ obniz.js
@@ -35127,9 +35127,10 @@ class NobleBindings extends events.EventEmitter {
 
       this.emit('disconnect', uuid); // TODO: handle reason?
     } else {
-      console.warn(
-        'noble warning: unknown handle ' + handle + ' disconnected!'
-      );
+      // maybe disconnect as peripheral
+      // console.warn(
+      //   'noble warning: unknown handle ' + handle + ' disconnected!'
+      // );
     }
   }
 
