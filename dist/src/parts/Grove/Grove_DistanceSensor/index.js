2b6d03dd6db945fb23501afe4158d6cdcac9988f
渡辺康平 <watanabekouhei@Kohei-MacPro.local>
2020-03-16 14:34:05 +0900

    update index.ts documents

--- dist/src/parts/Grove/Grove_DistanceSensor/index.js
+++ dist/src/parts/Grove/Grove_DistanceSensor/index.js
@@ -3,73 +3,31 @@
  * @packageDocumentation
  * @module Parts..Grove_DistanceSensor
  */
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-class Grove_DistanceSensor {
-    constructor() {
-        this._unit = "mm";
-        this.keys = ["vcc", "gnd", "signal", "grove"];
-        this.requiredKeys = [];
-    }
+const GP2Y0A21YK0F_1 = __importDefault(require("../../../parts/DistanceSensor/GP2Y0A21YK0F"));
+class Grove_DistanceSensor extends GP2Y0A21YK0F_1.default {
     static info() {
         return {
             name: "Grove_DistanceSensor",
         };
     }
+    constructor() {
+        super();
+        this.keys = ["gnd", "vcc", "signal", "grove"];
+        this.requiredKeys = [];
+    }
     wired(obniz) {
         this.obniz = obniz;
         if (this.params.grove) {
             const groveAd = this.params.grove.getAnalog();
-            this.ad = groveAd.primary;
-            console.log(this.ad);
+            this.ad_signal = groveAd.secondary;
         }
         else {
             this.obniz.setVccGnd(this.params.vcc, this.params.gnd, "5v");
-            this.ad = obniz.getAD(this.params.signal);
-        }
-    }
-    start(callback) {
-        this.ad.start((val) => {
-            const distance = this._volt2distance(val);
-            if (typeof callback === "function") {
-                callback(distance);
-            }
-        });
-    }
-    _volt2distance(val) {
-        if (val <= 0) {
-            val = 0.001;
-        }
-        let distance = 19988.34 * Math.pow((val / 5.0) * 1024, -1.25214) * 10;
-        if (this._unit === "mm") {
-            distance = Math.floor(distance * 10) / 10;
-        }
-        else {
-            distance *= 0.0393701;
-            distance = Math.floor(distance * 1000) / 1000;
-        }
-        return distance;
-    }
-    getWait() {
-        return new Promise(async (resolve, reject) => {
-            try {
-                const val = await this.ad.getWait();
-                const distance = this._volt2distance(val);
-                resolve(distance);
-            }
-            catch (e) {
-                reject(e);
-            }
-        });
-    }
-    unit(unit) {
-        if (unit === "mm") {
-            this._unit = "mm";
-        }
-        else if (unit === "inch") {
-            this._unit = "inch";
-        }
-        else {
-            throw new Error("unknown unit " + unit);
+            this.ad_signal = obniz.getAD(this.params.signal);
         }
     }
 }
--- src/parts/Grove/Grove_DistanceSensor/index.ts
+++ src/parts/Grove/Grove_DistanceSensor/index.ts
@@ -4,9 +4,9 @@
  */
 
 import Obniz from "../../../obniz";
-import PeripheralAD from "../../../obniz/libs/io_peripherals/ad";
 import PeripheralGrove from "../../../obniz/libs/io_peripherals/grove";
-import ObnizPartsInterface, { ObnizPartsInfo } from "../../../obniz/ObnizPartsInterface";
+import { ObnizPartsInfo } from "../../../obniz/ObnizPartsInterface";
+import GP2Y0A21YK0F from "../../../parts/DistanceSensor/GP2Y0A21YK0F";
 
 interface Grove_DistanceSensorOptionsA {
   gnd?: number;
@@ -18,27 +18,18 @@ interface Grove_DistanceSensorOptionsB {
   grove: PeripheralGrove;
 }
 
-export type Grove_DistanceSensorUnitType = "mm" | "inch";
-
 export type Grove_DistanceSensorOptions = Grove_DistanceSensorOptionsA | Grove_DistanceSensorOptionsB;
 
-export default class Grove_DistanceSensor implements ObnizPartsInterface {
+export default class Grove_DistanceSensor extends GP2Y0A21YK0F {
   public static info(): ObnizPartsInfo {
     return {
       name: "Grove_DistanceSensor",
     };
   }
 
-  public requiredKeys: string[];
-  public keys: string[];
-  public params: any;
-
-  public _unit: Grove_DistanceSensorUnitType = "mm";
-  public ad!: PeripheralAD;
-  protected obniz!: Obniz;
-
   constructor() {
-    this.keys = ["vcc", "gnd", "signal", "grove"];
+    super();
+    this.keys = ["gnd", "vcc", "signal", "grove"];
     this.requiredKeys = [];
   }
 
@@ -46,57 +37,10 @@ export default class Grove_DistanceSensor implements ObnizPartsInterface {
     this.obniz = obniz;
     if (this.params.grove) {
       const groveAd = this.params.grove.getAnalog();
-      this.ad = groveAd.primary;
-      console.log(this.ad);
+      this.ad_signal = groveAd.secondary;
     } else {
       this.obniz.setVccGnd(this.params.vcc, this.params.gnd, "5v");
-      this.ad = obniz.getAD(this.params.signal);
-    }
-  }
-
-  public start(callback: (distance: number) => void) {
-    this.ad.start((val: number) => {
-      const distance = this._volt2distance(val);
-      if (typeof callback === "function") {
-        callback(distance);
-      }
-    });
-  }
-
-  public _volt2distance(val: number): number {
-    if (val <= 0) {
-      val = 0.001;
-    }
-
-    let distance = 19988.34 * Math.pow((val / 5.0) * 1024, -1.25214) * 10;
-    if (this._unit === "mm") {
-      distance = Math.floor(distance * 10) / 10;
-    } else {
-      distance *= 0.0393701;
-      distance = Math.floor(distance * 1000) / 1000;
-    }
-    return distance;
-  }
-
-  public getWait(): Promise<number> {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const val = await this.ad.getWait();
-        const distance = this._volt2distance(val);
-        resolve(distance);
-      } catch (e) {
-        reject(e);
-      }
-    });
-  }
-
-  public unit(unit: Grove_DistanceSensorUnitType) {
-    if (unit === "mm") {
-      this._unit = "mm";
-    } else if (unit === "inch") {
-      this._unit = "inch";
-    } else {
-      throw new Error("unknown unit " + unit);
+      this.ad_signal = obniz.getAD(this.params.signal);
     }
   }
 }
