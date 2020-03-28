b01017c394b6b169f144484c36d3dd1ce5e70095
yukisato <yuki@yuki-sato.com>
2020-03-28 16:37:40 +0900

    update alpha

--- dist/package.js
+++ dist/package.js
@@ -1,6 +1,6 @@
 module.exports = {
   "name": "obniz",
-  "version": "3.4.0",
+  "version": "3.5.0-alpha.2",
   "description": "obniz sdk for javascript",
   "main": "./dist/src/obniz/index.js",
   "types": "./dist/src/obniz/index.d.ts",
