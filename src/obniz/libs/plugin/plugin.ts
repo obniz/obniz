3504d3385e206c269c813c973441cfb71dc0980d
木戸 康平 <bouningenn@gmail.com>
2020-07-20 15:37:36 +0900

    update plugin/receive function like uart

--- src/obniz/libs/plugin/plugin.ts
+++ src/obniz/libs/plugin/plugin.ts
@@ -5,12 +5,13 @@
 
 import semver from "semver";
 import Obniz from "../../index";
+import ObnizUtil from "../utils/util";
 
 /**
  * @param PluginReceiveCallbackFunction.data
  * received data
  */
-type PluginReceiveCallbackFunction = (data: number[]) => void;
+type PluginReceiveCallbackFunction = (data: number[], str: string | null) => void;
 
 export default class Plugin {
   /**
@@ -70,6 +71,7 @@ export default class Plugin {
    * @private
    */
   public _reset() {}
+
   /**
    * @ignore
    * @param obj
@@ -77,9 +79,8 @@ export default class Plugin {
   public notified(obj: any) {
     if (obj.receive) {
       /* Connectino state update. response of connect(), close from destination, response from */
-      if (this.onreceive) {
-        this.onreceive(obj.receive);
-      }
+      const string = ObnizUtil.dataArray2string(obj.receive);
+      this.Obniz._runUserCreatedFunction(this.onreceive, obj.receive, string);
     }
   }
 }
