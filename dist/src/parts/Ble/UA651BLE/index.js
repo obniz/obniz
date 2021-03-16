61b2274236bfa401fbb0db2a15c2661a82487c50
KazukiZjalic <kazukizjalic@gmail.com>
2021-03-16 19:59:31 +0900

    UA651BLEのライブラリ完成(血圧，脈拍取得，時刻は取得不可・原因不明)

--- dist/src/parts/Ble/UA651BLE/index.js
+++ dist/src/parts/Ble/UA651BLE/index.js
@@ -56,10 +56,10 @@ class UA651BLE {
                 throw new Error("UA651BLE not found");
             }
             const results = [];
-            const { temperatureMeasurementChar, timeChar, customServiceChar } = this._getChars();
+            const { bloodPressureMeasurementChar, timeChar, customServiceChar } = this._getChars();
             await customServiceChar.writeWait([2, 0, 0xe1]); // send all data
             await this._writeTimeChar(this._timezoneOffsetMinute);
-            temperatureMeasurementChar.registerNotifyWait((data) => {
+            bloodPressureMeasurementChar.registerNotifyWait((data) => {
                 results.push(this._analyzeData(data));
             });
             this._peripheral.ondisconnect = (reason) => {
@@ -67,7 +67,7 @@ class UA651BLE {
             };
         });
     }
-    _readFloatLE(buffer, index) {
+    _readFLOAT_LE(buffer, index) {
         const data = buffer.readUInt32LE(index);
         let mantissa = data & 0x00ffffff;
         if ((mantissa & 0x00800000) > 0) {
@@ -76,23 +76,79 @@ class UA651BLE {
         const exponential = data >> 24;
         return mantissa * Math.pow(10, exponential);
     }
+    _readSFLOAT_LE(buffer, index) {
+        const data = buffer.readUInt16LE(index);
+        let mantissa = data & 0x0fff;
+        if ((mantissa & 0x0800) > 0) {
+            mantissa = -1 * (~(mantissa - 0x01) & 0x0fff);
+        }
+        const exponential = data >> 12;
+        return mantissa * Math.pow(10, exponential);
+    }
     _analyzeData(data) {
         const buf = Buffer.from(data);
         const flags = buf.readUInt8(0);
         let index = 1;
         const result = {};
+        // if (flags & 0x01) {
+        //   // Fahrenheit
+        //   result.fahrenheit = this._readFloatLE(buf, index);
+        //   index += 4;
+        // } else {
+        //   // Celsius
+        //   result.celsius = this._readFloatLE(buf, index);
+        //   index += 4;
+        // }
+        // if (flags & 0x02) {
+        //   // Time Stamp field present
+        //   result.date = {
+        //     year: buf.readUInt16LE(index),
+        //     month: buf.readUInt8(index + 2),
+        //     day: buf.readUInt8(index + 3),
+        //     hour: buf.readUInt8(index + 4),
+        //     minute: buf.readUInt8(index + 5),
+        //     second: buf.readUInt8(index + 6),
+        //   };
+        //   index += 7;
+        // }
+        // if (flags & 0x04) {
+        //   const types = [
+        //     "unknown",
+        //     "Armpit",
+        //     "Body",
+        //     "Ear",
+        //     "Finger",
+        //     "Gastro-intestinal Tract",
+        //     "Mouth",
+        //     "Rectum",
+        //     "Toe",
+        //     "Tympanum",
+        //   ];
+        //   const value = buf.readUInt8(index);
+        //   index++;
+        //   result.temperatureType = types[value] || "unknown";
+        // }
         if (flags & 0x01) {
-            // Fahrenheit
-            result.fahrenheit = this._readFloatLE(buf, index);
-            index += 4;
+            // Blood Pressure Unit Flag
+            // kPa
+            result.SystolicPressure_kPa = this._readSFLOAT_LE(buf, index);
+            index += 2;
+            result.DiastolicPressure_kPa = this._readSFLOAT_LE(buf, index);
+            index += 2;
+            result.MeanArterialPressure_kPa = this._readSFLOAT_LE(buf, index);
+            index += 2;
         }
         else {
-            // Celsius
-            result.celsius = this._readFloatLE(buf, index);
-            index += 4;
+            // mmHg
+            result.SystolicPressure_mmHg = this._readSFLOAT_LE(buf, index);
+            index += 2;
+            result.DiastolicPressure_mmHg = this._readSFLOAT_LE(buf, index);
+            index += 2;
+            result.MeanArterialPressure_mmHg = this._readSFLOAT_LE(buf, index);
+            index += 2;
         }
         if (flags & 0x02) {
-            // Time Stamp field present
+            // Time Stamp Flag
             result.date = {
                 year: buf.readUInt16LE(index),
                 month: buf.readUInt8(index + 2),
@@ -104,21 +160,9 @@ class UA651BLE {
             index += 7;
         }
         if (flags & 0x04) {
-            const types = [
-                "unknown",
-                "Armpit",
-                "Body",
-                "Ear",
-                "Finger",
-                "Gastro-intestinal Tract",
-                "Mouth",
-                "Rectum",
-                "Toe",
-                "Tympanum",
-            ];
-            const value = buf.readUInt8(index);
-            index++;
-            result.temperatureType = types[value] || "unknown";
+            // Pulse Rate Flag
+            result.PulseRate = this._readSFLOAT_LE(buf, index);
+            index += 2;
         }
         return result;
     }
@@ -126,15 +170,27 @@ class UA651BLE {
         if (!this._peripheral) {
             throw new Error("UA651BLE not found");
         }
-        const temperatureMeasurementChar = this._peripheral
-            .getService("1809")
-            .getCharacteristic("2A1C");
-        const timeChar = this._peripheral.getService("1809").getCharacteristic("2A08");
+        // const temperatureMeasurementChar: BleRemoteCharacteristic = this._peripheral
+        //   .getService("1809")!
+        //   .getCharacteristic("2A1C")!;
+        // const timeChar = this._peripheral.getService("1809")!.getCharacteristic("2A08")!;
+        // const customServiceChar = this._peripheral
+        //   .getService("233bf0005a341b6d975c000d5690abe4")!
+        //   .getCharacteristic("233bf0015a341b6d975c000d5690abe4")!;
+        // return {
+        //   temperatureMeasurementChar,
+        //   timeChar,
+        //   customServiceChar,
+        // };
+        const bloodPressureMeasurementChar = this._peripheral
+            .getService("1810")
+            .getCharacteristic("2A35");
+        const timeChar = this._peripheral.getService("1810").getCharacteristic("2A08");
         const customServiceChar = this._peripheral
-            .getService("233bf0005a341b6d975c000d5690abe4")
-            .getCharacteristic("233bf0015a341b6d975c000d5690abe4");
+            .getService("233bf0005a341b6d975c000d5690abe4") // Primary Service Custom Service(pp.14)
+            .getCharacteristic("233bf0015a341b6d975c000d5690abe4"); // Custom Characteristic(pp.14)
         return {
-            temperatureMeasurementChar,
+            bloodPressureMeasurementChar,
             timeChar,
             customServiceChar,
         };
--- src/parts/Ble/UA651BLE/index.ts
+++ src/parts/Ble/UA651BLE/index.ts
@@ -12,9 +12,25 @@ import BleGenericAccess from "../abstract/services/genericAccess";
 export interface UA651BLEOptions {}
 
 export interface UA651BLEResult {
-  fahrenheit?: number;
-  celsius?: number;
+  // fahrenheit?: number;
+  // celsius?: number;
+  // date?: {
+  //   year: number;
+  //   month: number;
+  //   day: number;
+  //   hour: number;
+  //   minute: number;
+  //   second: number;
+  // };
+  // temperatureType?: string;
+  SystolicPressure_mmHg?: number; // ex) 128mmHg → 0x80 = 128, 0x00
+  DiastolicPressure_mmHg?: number;
+  MeanArterialPressure_mmHg?: number;
+  SystolicPressure_kPa?: number; // ex) 17.6Kpa → 0xB0 = 176, 0xF0
+  DiastolicPressure_kPa?: number;
+  MeanArterialPressure_kPa?: number;
   date?: {
+    // Time Stamp ex) 2013/8/26 9:10:20 → 0xDD 0x07 0x08 0x1A 0x09 0x0A 0x14
     year: number;
     month: number;
     day: number;
@@ -22,7 +38,7 @@ export interface UA651BLEResult {
     minute: number;
     second: number;
   };
-  temperatureType?: string;
+  PulseRate?: number;
 }
 
 export default class UA651BLE implements ObnizPartsBleInterface {
@@ -93,13 +109,13 @@ export default class UA651BLE implements ObnizPartsBleInterface {
         throw new Error("UA651BLE not found");
       }
       const results: UA651BLEResult[] = [];
-      const { temperatureMeasurementChar, timeChar, customServiceChar } = this._getChars();
+      const { bloodPressureMeasurementChar, timeChar, customServiceChar } = this._getChars();
 
       await customServiceChar.writeWait([2, 0, 0xe1]); // send all data
 
       await this._writeTimeChar(this._timezoneOffsetMinute);
 
-      temperatureMeasurementChar.registerNotifyWait((data: number[]) => {
+      bloodPressureMeasurementChar.registerNotifyWait((data: number[]) => {
         results.push(this._analyzeData(data));
       });
 
@@ -109,7 +125,7 @@ export default class UA651BLE implements ObnizPartsBleInterface {
     });
   }
 
-  private _readFloatLE(buffer: Buffer, index: number) {
+  private _readFLOAT_LE(buffer: Buffer, index: number) {
     const data = buffer.readUInt32LE(index);
     let mantissa = data & 0x00ffffff;
     if ((mantissa & 0x00800000) > 0) {
@@ -119,23 +135,80 @@ export default class UA651BLE implements ObnizPartsBleInterface {
     return mantissa * Math.pow(10, exponential);
   }
 
+  private _readSFLOAT_LE(buffer: Buffer, index: number) {
+    const data = buffer.readUInt16LE(index);
+    let mantissa = data & 0x0fff;
+    if ((mantissa & 0x0800) > 0) {
+      mantissa = -1 * (~(mantissa - 0x01) & 0x0fff);
+    }
+    const exponential = data >> 12;
+    return mantissa * Math.pow(10, exponential);
+  }
+
   private _analyzeData(data: number[]): UA651BLEResult {
     const buf = Buffer.from(data);
     const flags = buf.readUInt8(0);
 
     let index = 1;
     const result: UA651BLEResult = {};
+    // if (flags & 0x01) {
+    //   // Fahrenheit
+    //   result.fahrenheit = this._readFloatLE(buf, index);
+    //   index += 4;
+    // } else {
+    //   // Celsius
+    //   result.celsius = this._readFloatLE(buf, index);
+    //   index += 4;
+    // }
+    // if (flags & 0x02) {
+    //   // Time Stamp field present
+    //   result.date = {
+    //     year: buf.readUInt16LE(index),
+    //     month: buf.readUInt8(index + 2),
+    //     day: buf.readUInt8(index + 3),
+    //     hour: buf.readUInt8(index + 4),
+    //     minute: buf.readUInt8(index + 5),
+    //     second: buf.readUInt8(index + 6),
+    //   };
+    //   index += 7;
+    // }
+    // if (flags & 0x04) {
+    //   const types = [
+    //     "unknown",
+    //     "Armpit",
+    //     "Body",
+    //     "Ear",
+    //     "Finger",
+    //     "Gastro-intestinal Tract",
+    //     "Mouth",
+    //     "Rectum",
+    //     "Toe",
+    //     "Tympanum",
+    //   ];
+    //   const value = buf.readUInt8(index);
+    //   index++;
+    //   result.temperatureType = types[value] || "unknown";
+    // }
     if (flags & 0x01) {
-      // Fahrenheit
-      result.fahrenheit = this._readFloatLE(buf, index);
-      index += 4;
+      // Blood Pressure Unit Flag
+      // kPa
+      result.SystolicPressure_kPa = this._readSFLOAT_LE(buf, index);
+      index += 2;
+      result.DiastolicPressure_kPa = this._readSFLOAT_LE(buf, index);
+      index += 2;
+      result.MeanArterialPressure_kPa = this._readSFLOAT_LE(buf, index);
+      index += 2;
     } else {
-      // Celsius
-      result.celsius = this._readFloatLE(buf, index);
-      index += 4;
+      // mmHg
+      result.SystolicPressure_mmHg = this._readSFLOAT_LE(buf, index);
+      index += 2;
+      result.DiastolicPressure_mmHg = this._readSFLOAT_LE(buf, index);
+      index += 2;
+      result.MeanArterialPressure_mmHg = this._readSFLOAT_LE(buf, index);
+      index += 2;
     }
     if (flags & 0x02) {
-      // Time Stamp field present
+      // Time Stamp Flag
       result.date = {
         year: buf.readUInt16LE(index),
         month: buf.readUInt8(index + 2),
@@ -147,21 +220,9 @@ export default class UA651BLE implements ObnizPartsBleInterface {
       index += 7;
     }
     if (flags & 0x04) {
-      const types = [
-        "unknown",
-        "Armpit",
-        "Body",
-        "Ear",
-        "Finger",
-        "Gastro-intestinal Tract",
-        "Mouth",
-        "Rectum",
-        "Toe",
-        "Tympanum",
-      ];
-      const value = buf.readUInt8(index);
-      index++;
-      result.temperatureType = types[value] || "unknown";
+      // Pulse Rate Flag
+      result.PulseRate = this._readSFLOAT_LE(buf, index);
+      index += 2;
     }
 
     return result;
@@ -172,16 +233,29 @@ export default class UA651BLE implements ObnizPartsBleInterface {
       throw new Error("UA651BLE not found");
     }
 
-    const temperatureMeasurementChar: BleRemoteCharacteristic = this._peripheral
-      .getService("1809")!
-      .getCharacteristic("2A1C")!;
-    const timeChar = this._peripheral.getService("1809")!.getCharacteristic("2A08")!;
+    // const temperatureMeasurementChar: BleRemoteCharacteristic = this._peripheral
+    //   .getService("1809")!
+    //   .getCharacteristic("2A1C")!;
+    // const timeChar = this._peripheral.getService("1809")!.getCharacteristic("2A08")!;
+    // const customServiceChar = this._peripheral
+    //   .getService("233bf0005a341b6d975c000d5690abe4")!
+    //   .getCharacteristic("233bf0015a341b6d975c000d5690abe4")!;
+
+    // return {
+    //   temperatureMeasurementChar,
+    //   timeChar,
+    //   customServiceChar,
+    // };
+    const bloodPressureMeasurementChar: BleRemoteCharacteristic = this._peripheral
+      .getService("1810")!
+      .getCharacteristic("2A35")!;
+    const timeChar = this._peripheral.getService("1810")!.getCharacteristic("2A08")!;
     const customServiceChar = this._peripheral
-      .getService("233bf0005a341b6d975c000d5690abe4")!
-      .getCharacteristic("233bf0015a341b6d975c000d5690abe4")!;
+      .getService("233bf0005a341b6d975c000d5690abe4")! // Primary Service Custom Service(pp.14)
+      .getCharacteristic("233bf0015a341b6d975c000d5690abe4")!; // Custom Characteristic(pp.14)
 
     return {
-      temperatureMeasurementChar,
+      bloodPressureMeasurementChar,
       timeChar,
       customServiceChar,
     };
