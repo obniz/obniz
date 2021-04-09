6fd0ac80079eef809322588d8404678319f1153b
木戸 康平 <bouningenn@gmail.com>
2021-04-09 12:05:49 +0900

    fix function name lint

--- devtools/eslint/test/async-function-name.js
+++ devtools/eslint/test/async-function-name.js
@@ -61,21 +61,21 @@ tester.run('async-function-name', require('../rule/async-function-name'), {
       parserOptions: {
         ecmaVersion: 2017,
       },
-      errors: ['Async function name "foo" without suffix "wait" was found.'],
+      errors: ['Async function name "foo" without suffix "Wait" was found.'],
     },
     {
       code: 'async function aaa( ){ }',
       parserOptions: {
         ecmaVersion: 2017,
       },
-      errors: ['Async function name "aaa" without suffix "wait" was found.'],
+      errors: ['Async function name "aaa" without suffix "Wait" was found.'],
     },
     {
       code: 'class A { async test(){} }',
       parserOptions: {
         ecmaVersion: 2017,
       },
-      errors: ['Async function name "test" without suffix "wait" was found.'],
+      errors: ['Async function name "test" without suffix "Wait" was found.'],
     },
   ],
 });
