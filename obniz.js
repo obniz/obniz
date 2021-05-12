340542a1c9adb8e69950cca77bf00dbe4ab0577f
木戸 康平 <bouningenn@gmail.com>
2021-05-12 22:42:47 +0900

    3.15.1

--- obniz.js
+++ obniz.js
@@ -92,7 +92,7 @@ var Obniz =
 
 module.exports = {
   "name": "obniz",
-  "version": "3.15.1-alpha.0",
+  "version": "3.15.1",
   "description": "obniz sdk for javascript",
   "main": "./dist/src/obniz/index.js",
   "types": "./dist/src/obniz/index.d.ts",
