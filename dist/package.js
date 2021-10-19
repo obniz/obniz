b770b65d0754faae179d56361782c58aefda9afa
木戸 康平 <bouningenn@gmail.com>
2021-10-19 15:30:58 +0900

    3.18.0-alpha.1

--- dist/package.js
+++ dist/package.js
@@ -1,6 +1,6 @@
 module.exports = {
   "name": "obniz",
-  "version": "3.18.0-alpha.0",
+  "version": "3.18.0-alpha.1",
   "description": "obniz sdk for javascript",
   "main": "./dist/src/obniz/index.js",
   "types": "./dist/src/obniz/index.d.ts",
