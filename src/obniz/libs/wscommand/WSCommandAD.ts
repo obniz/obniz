f6c44c0c0f314fb349499e9c57331565613a3009
yukisato <yuki@yuki-sato.com>
2021-11-25 20:08:17 +0900

    adopt for cc3235 ad

--- src/obniz/libs/wscommand/WSCommandAD.ts
+++ src/obniz/libs/wscommand/WSCommandAD.ts
@@ -63,20 +63,27 @@ class WSCommandAD extends WSCommand {
     if (func === this._CommandNotifyValue) {
       for (let i = 0; i + 2 < payload.byteLength; i += 3) {
         let value: number;
-        if (payload[i + 1] & 0x80) {
-          // 10bit mode
-          value = ((payload[i + 1] & 0x03) << 8) + payload[i + 2]; // 0x0000 to 0x3FF;
-          value = (5.0 * value) / 1023.0; // 1023.0 ===0x3FF
-          value = Math.round(value * 1000) / 1000;
-        } else if (payload[i + 1] & 0x40) {
+        if (this._hw.hw === 'cc3235mod') {
           // 12bit mode
           value = ((payload[i + 1] & 0x0f) << 8) + payload[i + 2]; // 0x0000 to 0x3FF;
-          value = (3.3 * value) / 4095.0; // 4095.0 ===0xFFF // vdd is not always 3.3v but...
+          value = (3.3 * value) / 4095.0; // 4095.0 ===0xFFF // vdd is not always
           value = Math.round(value * 1000) / 1000;
         } else {
-          // unsigned 100 times mode. (0 to 500 from 0v to 5v).
-          value = (payload[i + 1] << 8) + payload[i + 2];
-          value = value / 100.0;
+          if (payload[i + 1] & 0x80) {
+            // 10bit mode
+            value = ((payload[i + 1] & 0x03) << 8) + payload[i + 2]; // 0x0000 to 0x3FF;
+            value = (5.0 * value) / 1023.0; // 1023.0 ===0x3FF
+            value = Math.round(value * 1000) / 1000;
+          } else if (payload[i + 1] & 0x40) {
+            // 12bit mode
+            value = ((payload[i + 1] & 0x0f) << 8) + payload[i + 2]; // 0x0000 to 0x3FF;
+            value = (3.3 * value) / 4095.0; // 4095.0 ===0xFFF // vdd is not always 3.3v but...
+            value = Math.round(value * 1000) / 1000;
+          } else {
+            // unsigned 100 times mode. (0 to 500 from 0v to 5v).
+            value = (payload[i + 1] << 8) + payload[i + 2];
+            value = value / 100.0;
+          }
         }
         objToSend['ad' + payload[i]] = value;
       }
