578f8ff158368e94cba0fa5b05f7ce572a5514f5
木戸 康平 <bouningenn@gmail.com>
2022-01-25 17:54:52 +0900

    add blewifi_gw2

--- src/obniz/libs/hw/index.ts
+++ src/obniz/libs/hw/index.ts
@@ -25,6 +25,8 @@ export default class HW {
       return require('./cc3235mod.json');
     } else if (hw === 'esp32c3') {
       return require('./esp32c3.json');
+    } else if (hw === 'blewifi_gw2') {
+      return require('./blewifi_gw2.json');
     } else {
       // default
       return require('./esp32w.json');
