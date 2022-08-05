f91d2dad3645782cd82086b8a7eea952361cc98e
SugimotoMasaki <77137241+SugimotoMasaki@users.noreply.github.com>
2022-08-05 15:12:31 +0900

    button sample

---
+++ examples/nodejs/peripherals/ble/sampleBU.js
@@ -0,0 +1,71 @@
+// const Obniz = require('obniz');
+const Obniz = require('../../../../index.js'); // local
+const mesh_bu = Obniz.getPartsClass('MESH_100BU');
+const MESH_100LE = Obniz.getPartsClass('MESH_100LE');
+const mesh_ac = Obniz.getPartsClass('MESH_100AC');
+
+const obnizId = '87287267';
+
+const obniz = new Obniz(obnizId, {
+  access_token: null,
+});
+
+// Connected.
+obniz.onconnect = async () => {
+  console.log(`connected obniz ${obniz.id}`);
+  try {
+    await obniz.ble.initWait();
+    obniz.ble.scan.onfind = async (peripheral) => {
+      sampleBU(peripheral);
+    };
+    await obniz.ble.scan.startWait();
+    // await continueScan(obniz);
+  } catch (e) {
+    if (e.name === 'ObnizOfflineError') {
+      // just disconnected. waiting for new connection establishment.
+    } else {
+      console.error(e);
+    }
+  }
+};
+
+// Disconnected.
+obniz.onclose = async () => {
+  console.log(`connection lost for obniz ${obniz.id}`);
+};
+
+async function sampleBU(peripheral) {
+  if (!mesh_bu.isMESHblock(peripheral)) {
+    return;
+  }
+  console.log('found : ' + peripheral.localName + ' : ' + peripheral.rssi);
+  const BU_block = new mesh_bu(peripheral);
+  await BU_block.connectWait();
+
+  let count = 0;
+  const goal = 10;
+
+  BU_block.onSinglePressed = (() => {
+    ++ count;
+    console.log('single pressed, plus 1; current = ' + count);
+    if (count === goal) { console.log('YOU WON !!');}
+  });
+
+  BU_block.onDoublePressed = (() => {
+    count += 2;
+    console.log('double pressed, plus 2; current = ' + count);
+    if (count === goal) { console.log('YOU WON !!');}
+  });
+
+  BU_block.onLongPressed = (() => {
+    count = 0;
+    console.log('long pressed, reset count; current = ' + count);
+  });
+
+  BU_block.onBatteryNotify = ((batt) => {
+    console.log('battery last : ' + batt + '/10');
+  });
+
+  const res = await BU_block.getDataWait();
+  console.log(res);
+}
