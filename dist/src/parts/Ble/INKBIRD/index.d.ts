5be964e82d4dcfaba1e982fbf19770847ffd7ecd
masudayasuto <masuda@ciebus.net>
2022-11-10 11:15:02 +0900

    fix: validate device

--- dist/src/parts/Ble/INKBIRD/index.d.ts
+++ dist/src/parts/Ble/INKBIRD/index.d.ts
@@ -43,7 +43,6 @@ export default class INKBIRD implements ObnizPartsBleInterface {
      * ```
      */
     static getData(peripheral: BleRemotePeripheral): INKBIRD_Data | null;
-    private static _deviceAdvAnalyzer;
     private static _deviceScanResponseAnalyzer;
     _peripheral: BleRemotePeripheral | null;
 }
