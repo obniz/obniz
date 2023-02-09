02f12b0bb40d6e0f85ae136fb8c5707c68ba906e
KoheiKido <bouningenn@gmail.com>
2023-02-09 22:42:43 +0900

    3.25.0

--- obniz.js
+++ obniz.js
@@ -92,7 +92,7 @@ var Obniz =
 
 module.exports = {
   "name": "obniz",
-  "version": "3.25.0-beta.1",
+  "version": "3.25.0",
   "description": "obniz sdk for javascript",
   "main": "./dist/src/obniz/index.js",
   "types": "./dist/src/obniz/index.d.ts",
